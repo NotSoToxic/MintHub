6252a6d0927b6e3253dc08556389994233ff26b0
Sarthak Joshi <93981003+NotSoToxic@users.noreply.github.com>
2023-07-13 08:48:26 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
 
+
 router.post("/", async (req, res) => {
 	try {
 		const { error } = validate(req.body);
