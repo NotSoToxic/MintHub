5f9dfb1fc9b77a86f72f351fabce8250e8d6cae7
Sarthak Joshi <93981003+NotSoToxic@users.noreply.github.com>
2023-08-01 07:55:34 +0530

    Update j.js

--- j.js
+++ j.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose')
 const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
 module.exports = function (callback) {
